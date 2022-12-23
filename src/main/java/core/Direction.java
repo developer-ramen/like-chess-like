80d818caf6916476c6862b258a74ca1da5695891
Developer Ramen <ramendev2009@gmail.com>
2022-12-23 22:59:01 +0700

    FORMAT: Fix amall formatting errors

--- src/main/java/core/Direction.java
+++ src/main/java/core/Direction.java
@@ -5,25 +5,24 @@ import java.util.List;
 public class Direction extends Point {
 	public Direction(int file, int rank) {
 		super(file, rank);
-	}	public static final Direction
+	}
+	
+	public Direction(Point point) {
+		super(point.getFile(), point.getRank());
+	}
+	
+	public static final Direction
 		NORTH = new Direction(-1, 0),
 		SOUTH = new Direction(1, 0),
 		WEST = new Direction(0, -1),
 		EAST = new Direction(0, 1),
-	
-	NW = new Direction(NORTH.add(WEST)),
+		NW = new Direction(NORTH.add(WEST)),
 		NE = new Direction(NORTH.add(EAST)),
 		SW = new Direction(SOUTH.add(WEST)),
 		SE = new Direction(SOUTH.add(EAST));
 	
-	public Direction(Point point) {
-		super(point.getFile(), point.getRank());
-	}	public static final List<Direction>
+	public static final List<Direction>
 		CARDINALS = List.of(NORTH, SOUTH, WEST, EAST),
 		DIAGONALS = List.of(NW, NE, SW, SE),
 		ALL = List.of(NORTH, SOUTH, WEST, EAST, NW, NE, SW, SE);
-	
-
-	
-
 }
